85762b72cd76ffef932d24d4800d012c5818c5ba
oproiumihai <oproiu.mihai95@gmail.com>
2020-03-11 11:43:55 +0200

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+gpio write 2 1
+sleep 1
+gpio write 30 1
+sleep 1
+gpio write 21 1
+sleep 1
+gpio write 22 1
+sleep 1
+gpio write 23 1
+sleep 1
+gpio write 24 1
+sleep 1
+gpio write 25 1
+sleep 1
+gpio write 1 1
+sleep 1
+gpio write 4 1
+sleep 1
+gpio write 5 1
+sleep 1
+gpio write 27 1
+sleep 1
+gpio write 28 1
+sleep 1
+gpio write 29 1
+sleep 1
+gpio write 2 0
+sleep 1
+gpio write 30 0
+sleep 1
+gpio write 21 0
+sleep 1
+gpio write 22 0
+sleep 1
+gpio write 23 0
+sleep 1
+gpio write 24 0
+sleep 1
+gpio write 25 0
+sleep 1
+gpio write 1 0
+sleep 1
+gpio write 4 0
+sleep 1
+gpio write 5 0
+sleep 1
+gpio write 27 0
+sleep 1
+gpio write 28 0
+sleep 1
+gpio write 29 0
+sleep 1
+
