d900cc6b4aaf8758584632424e61754ae0ca09ea
oproiumihai <oproiu.mihai95@gmail.com>
2020-03-11 11:44:09 +0200

    Create stare_inundatie.php

---
+++ stare_inundatie.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "1211";
+$dbname = "raspberry";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT `id`, `stare` FROM `inundatie` ORDER BY `id` DESC LIMIT 1";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+	
+    $row = $result->fetch_assoc();
+    echo $row["stare"];
+} else {
+    echo "0";
+}
+$conn->close();
+
+?>
