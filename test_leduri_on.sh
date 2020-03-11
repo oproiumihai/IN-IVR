7b8f3b72cdd0c56b7186f0fd94d95d83a62c9a76
oproiumihai <oproiu.mihai95@gmail.com>
2020-03-11 11:43:49 +0200

    Create test_leduri_on.sh

---
+++ test_leduri_on.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+gpio write 2 1
+sleep 1
+gpio write 30 1
+sleep 1
+gpio write 21 1
+sleep 1
+gpio write 22 1
+sleep 1
+gpio write 23 1
+sleep 1
+gpio write 24 1
+sleep 1
+gpio write 25 1
+sleep 1
+gpio write 1 1
+sleep 1
+gpio write 4 1
+sleep 1
+gpio write 5 1
+sleep 1
+gpio write 27 1
+sleep 1
+gpio write 28 1
+sleep 1
+gpio write 29 1
